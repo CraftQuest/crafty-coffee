f4ada5cb249b7f5c591a115251a7140922b855ba
Ryan Irelan <ryan@mijingo.com>
2018-10-26 09:20:53 -0500

    adding project config test

--- config/general.php
+++ config/general.php
@@ -23,6 +23,7 @@ return [
 
         // The secure key Craft will use for hashing and encrypting data
         'securityKey' => getenv('SECURITY_KEY'),
+        'useProjectConfigFile' => true,
     ],
 
     // Dev environment settings
